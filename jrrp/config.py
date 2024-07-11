cd1aca3e1b37143cb3ca763a857e688d4ea1d1d6
Sheng Fan <fredtools999@gmail.com>
2024-07-11 17:40:17 +0800

    refactor code to pass type checking

--- jrrp/config.py
+++ jrrp/config.py
@@ -4,52 +4,42 @@ from mcdreforged.api.utils.serializer import Serializable
 
 
 class JrrpConfig(Serializable):
-	online_mode: bool = True
-	start: str = "你今天的人品值是："
-	end: str = ""
-	title: bool = False
-	command: List[str] = ["!!jrrp"]
-	message: List[dict] = [
-		{
-			"expr": "jrrp == 100",
-			"start": "！！！！！你今天的人品值是：",
-			"end": "！100！100！！！！！",
-			"title": True
-		},
-		{
-			"expr": "jrrp == 99",
-			"end": "！但不是 100……"
-		},
-		{
-			"expr": "jrrp >= 90",
-			"end": "！好评如潮！"
-		},
-		{
-			"expr": "jrrp >= 60",
-			"end": "！是不错的一天呢！"
-		},
-		{
-			"expr": "jrrp > 50",
-			"end": "！还行啦还行啦。"
-		},
-		{
-			"expr": "jrrp == 50",
-			"end": "！五五开……"
-		},
-		{
-			"expr": "jrrp >= 40",
-			"end": "！还……还行吧……？"
-		},
-		{
-			"expr": "jrrp >= 11",
-			"end": "！呜哇……"
-		},
-		{
-			"expr": "jrrp >= 1",
-			"end": "……（没错，是百分制）",
-		},
-		{
-			"expr": "True",
-			"end": "……"
-		}
-	]
+    online_mode: bool = True
+    start: str = "你今天的人品值是："
+    end: str = ""
+    title: bool = False
+    command: List[str] = ["!!jrrp"]
+    stop_once_match: bool = True
+    message: List[dict] = [{
+        "expr": "jrrp == 100",
+        "start": "！！！！！你今天的人品值是：",
+        "end": "！100！100！！！！！",
+        "title": True
+    }, {
+        "expr": "jrrp == 99",
+        "end": "！但不是 100……"
+    }, {
+        "expr": "99 > jrrp >= 90",
+        "end": "！好评如潮！"
+    }, {
+        "expr": " 90 > jrrp >= 60",
+        "end": "！是不错的一天呢！"
+    }, {
+        "expr": "60 > jrrp > 50",
+        "end": "！还行啦还行啦。"
+    }, {
+        "expr": "jrrp == 50",
+        "end": "！五五开……"
+    }, {
+        "expr": "50 > jrrp >= 40",
+        "end": "！还……还行吧……？"
+    }, {
+        "expr": "40 > jrrp >= 11",
+        "end": "！呜哇……"
+    }, {
+        "expr": "10 >= jrrp >= 1",
+        "end": "……（没错，是百分制）",
+    }, {
+        "expr": "True",
+        "end": "……"
+    }]
--- jrrp/entry.py
+++ jrrp/entry.py
@@ -1,43 +1,64 @@
-from datetime import datetime
 import os.path
+from datetime import datetime
 
-from mcdreforged.api.types import PluginServerInterface, PlayerCommandSource
 from mcdreforged.api.command import Literal
+from mcdreforged.api.types import (CommandSource, PlayerCommandSource,
+                                   PluginServerInterface)
+
+import mc_uuid
 
 from .config import JrrpConfig
 
 
-def get_jrrp(string: str):
+def get_todays_luck(string: str):
     now = datetime.now()
-    num1 = round((abs((hash("asdfgbn" + str(now.timetuple().tm_yday)+ str(now.year) + "XYZ") / 3.0 + hash("QWERTY" + string + "0*8&6" + str(now.day) + "kjhg") / 3.0) / 527.0) % 1001.0))
+    num1 = round((abs(
+        (hash("asdfgbn" + str(now.timetuple().tm_yday) + str(now.year) + "XYZ")
+         / 3.0 + hash("QWERTY" + string + "0*8&6" + str(now.day) + "kjhg") /
+         3.0) / 527.0) % 1001.0))
     num2 = round(num1 / 969.0 * 99.0) if num1 < 970 else 100
     return num2
 
 
 def register_jrrp_command(server: PluginServerInterface):
-    def reply_jrrp(src: PlayerCommandSource):
-        uuid = mc_uuid.onlineUUID(src.player).hex if config.online_mode else mc_uuid.offlineUUID(src.player).hex
-        jrrp = get_jrrp(uuid)
-        for msg_obj in config.message:
-            if eval(msg_obj["expr"]):
-                start = msg_obj.get("start") if msg_obj.get("start") else config.start
-                end = msg_obj.get("end") if msg_obj.get("end") else config.end
-                title = msg_obj.get("title") if msg_obj.get("title") else config.title
-                msg = start + str(jrrp) + end
-                if title:
-                    src.get_server().execute("title {} {}".format(src.player, msg))
-                src.reply(msg)
 
     config = server.load_config_simple(os.path.join("config", "jrrp.json"),
                                        in_data_folder=False,
                                        target_class=JrrpConfig)
-    mc_uuid = server.get_plugin_instance("mc_uuid")
+
+    # target_class set so config is instance of JrrpConfig
+    if not isinstance(config, JrrpConfig):
+        config = JrrpConfig.deserialize(config)
+
+    def reply_todays_luck(src: CommandSource):
+        if not isinstance(src, PlayerCommandSource):
+            return
+
+        online_res = mc_uuid.onlineUUID(src.player)
+        player_uuid = online_res.hex if config.online_mode and online_res is not None else None
+        if player_uuid is None:
+            offline_res = mc_uuid.offlineUUID(src.player)
+            if offline_res is None:
+                raise RuntimeError("Invalid Player Name: {}".format(
+                    src.player))
+            player_uuid = offline_res.hex
+
+        jrrp = get_todays_luck(player_uuid)
+        for msg_obj in config.message:
+            if eval(msg_obj["expr"]):
+                prefix = msg_obj.get("start") or config.start
+                suffix = msg_obj.get("end") or config.end
+                title = msg_obj.get("title") or config.title
+                msg = prefix + str(jrrp) + suffix
+                if title:
+                    src.get_server().execute("title {} {}".format(
+                        src.player, msg))
+                src.reply(msg)
+
     for command in config.command:
         server.register_command(
-            Literal(command)
-            .requires(lambda src: src.is_player)
-            .runs(reply_jrrp)
-        )
+            Literal(command).requires(lambda src: src.is_player).runs(
+                reply_todays_luck))
 
 
 def on_load(server: PluginServerInterface, old):
